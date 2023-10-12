97613b3abe243dc7a1c5fe2f33d202bff13aa43e
Logan Bunch <loganbunch@gmail.com>
2023-10-12 05:50:29 +0000

    tina client info

--- tina/config.js
+++ tina/config.js
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
+  clientId: "4528f2b1-ff48-45ab-a476-513b73c565a1", // Get this from tina.io
+  token: "d9d7c799694bf0e80dbc30fc7762d8d0c0321ca5", // Get this from tina.io
 
   build: {
     outputFolder: "admin",
